8d911498553eb098e02d3ddcae3621d3b3d6abe7
Steven Xia <33969208+brutalsavage@users.noreply.github.com>
2020-03-10 01:44:04 -0400

    :alembic: utilize the kernel method with updated accuracy

--- Baseline/baseline.py
+++ Baseline/baseline.py
@@ -2,49 +2,66 @@ import manga109api
 from PIL import Image, ImageDraw
 import numpy as np
 import os
-
+import statistics
 
 # Calculate pixel accuracy for a single image prediction
 def pixel_accuracy(prediction=None, labeled=None):
 
-    total_pixels = np.where(labeled == 255)[0].size
-    if total_pixels == 0:
-        return 1
-    count = np.where((prediction==255) & (labeled==255))[0].size
-    acc = count/total_pixels
+    # intersection/union-intersection
 
+    intersection = np.where((prediction==255) & (labeled==255))[0].size
+    #intersection = np.count_nonzero((prediction==255) & (labeled==255))
+    union = np.where((prediction==255))[0].size+np.where((labeled==255))[0].size - intersection
+    acc = intersection/union
+    #print(acc)
     return acc
 
+
+
+kernal_inside = np.zeros((40,40))
+kernal_inside[25:35, 25:35] = 1
+kernal_outside = np.zeros((40,40))
+kernal_outside = kernal_outside+1
+kernal_outside[25:35, 25:35] = 0
 
 # Predict the image mask of the manga input
 def predict_sb(input):
 
-    pd = label = np.zeros((1170, 827))
+    pd = label = np.zeros((448, 320))
 
     # Force it to be between [0, 1]
     input = input/255
+    ones = input > 0.6
+    input[ones] = 1
+    zeros = input < 0.4
+    input[zeros] = 0
 
-    #x, y = 0, 0
-    kernal_x, kernal_y = 60, 60
+    kernal_x, kernal_y = 40, 40
     total_pixels = kernal_x*kernal_y
-    for x in range(0, 827, kernal_x):
-        for y in range(0, 1170, kernal_y):
+    for x in range(0, 320-40, 10):
+        for y in range(0, 448-40,10):
+
             temp = input[y:y+kernal_y, x:x+kernal_x]
+            outside = np.multiply(temp, kernal_outside)
+            inside = np.multiply(temp, kernal_inside)
 
-            white_pixels = np.where(temp>=0.8)[0].size/total_pixels
-            black_pixels = np.where(temp<=0.2)[0].size/total_pixels
+            inside_pixels = np.where(inside==0)[0].size/(10*10)
+            outside_pixels = np.where(outside==1)[0].size/total_pixels
 
-            if white_pixels > 0.2 and black_pixels > 0.2:
+            if outside_pixels > 0.8 and inside_pixels > 0.8:
                 pd[y:y+kernal_y, x:x+kernal_x] = 255
 
     return pd
 
 
-cleaned_path = "../data cleaning/cleaned mangas/"
-label_path = "../data cleaning/manga masks/"
+cleaned_path = "../data cleaning/small mangas/"
+label_path = "../data cleaning/small masks/"
 
-list_of_mangas = os.listdir(cleaned_path)
+list_of_mangas = os.listdir(cleaned_path)[0:2]
 k = 0
+
+hist_accuracy = []
+
 for mangas in list_of_mangas:
     list_of_images = os.listdir(cleaned_path+mangas+"/")
     for image_name in list_of_images:
@@ -55,8 +72,17 @@ for mangas in list_of_mangas:
         label = np.array(label_image)
 
         prediction = predict_sb(input)
-
+        # label1 = Image.fromarray(prediction)
+        # label1.show()
+        #
+        # label2 = Image.fromarray(label)
+        # label2.show()
+        #exit()
         accuracy = pixel_accuracy(prediction=prediction, labeled=label)
         print(accuracy)
+        hist_accuracy.append(accuracy)
+
+print(statistics.mean(hist_accuracy))
+print(max(hist_accuracy))
 
 
